165a3a0d10cb67f911649a3222a67fb5f0ad1862
MRO <miguel.r@jcaelectronics.ca>
2020-05-23 13:20:49 -0500

    funcion para regresar el tiempo restante ha sido agregada

--- PowerBoard/include/uvsafe_time.h
+++ PowerBoard/include/uvsafe_time.h
@@ -3,21 +3,21 @@
 *  Todos los derechos reservados
 *  Aguascalientes, Mexico. Mayo 2020
 */
+
 class Countdown{
   //modes
   int STOP = 0;
   int START = 1;
   bool isRunning = false;
 
-typedef void(*timer_callback)(void);
-
+  typedef void(*timer_callback)(void);
 
   int mode = STOP; //timer mode
   unsigned long time_period = 0; //Tiempo que el usuario desea contar
   unsigned long left_time = 0; //Tiempo restante
   unsigned long delta_time = 1000; //unidad minima que se descuenta al timer en segundos
   unsigned long last_update = 0;
-  timer_callback onComplete;
+  timer_callback onComplete; //esta es la funcion que se llama cuando el tiempo llega a cero
 
   public:
   Countdown(unsigned long _time_period, timer_callback _onComplete ){
@@ -25,14 +25,12 @@ typedef void(*timer_callback)(void);
       left_time = time_period;
       last_update = millis();
       onComplete = _onComplete;
-
     }
 
-  void run(){
 
+  void run(){
     if ((millis() - last_update) > delta_time){
       last_update = millis();
-      Serial.println(left_time-1);
       if (mode == START){
         left_time -= 1; //restar un segundo al tiempo restante
           }//MODE==start
@@ -42,28 +40,36 @@ typedef void(*timer_callback)(void);
         left_time = time_period;
         onComplete(); //call the callback
     }
-
   }
+
 
   void start(){
     mode = START;
     isRunning = true;
   }
 
+
   void stop(){
     mode = STOP;
     isRunning = false;
   }
 
+
   void pause(){
     mode = STOP; //but it is still running
   }
+
 
   void reset(){
     mode = STOP;
     left_time = time_period;
     isRunning = false;
   }
+
+  unsigned long get_time(){
+      return left_time;
+  }
+
 
   bool is_running(){
     return isRunning;
