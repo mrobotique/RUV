f6c369ad6204f3aa7ae62741954c3b59fd13be47
Miguel ROMERO <miguel.aguas@gmail.com>
2020-07-04 17:19:02 -0500

    Updates encoder and hexagon on display (UI)

--- UI/gui_st7920_arduino_nano/src/main.cpp
+++ UI/gui_st7920_arduino_nano/src/main.cpp
@@ -1,11 +1,17 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <SPI.h>
-
+#include "encoder.h"
 U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R2, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8);
 
 void setup() {
   u8g2.begin();
+  pinMode(pinA, INPUT_PULLUP); // set pinA as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+  pinMode(pinB, INPUT_PULLUP); // set pinB as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+  pinMode(selectSwitch, INPUT_PULLUP);
+  attachInterrupt(0, PinA, RISING); // set an interrupt on PinA, looking for a rising edge signal and executing the "PinA" Interrupt Service Routine (below)
+  attachInterrupt(1, PinB, RISING); // set an interrupt on PinB, looking for a rising edge signal and executing the "PinB" Interrupt Service Routine (below)
+  Serial.begin(115200); // start the serial monitor link
 }
 
 void u8g2_prepare(void) {
@@ -62,13 +68,18 @@ void draw_hexagon(int x_offset){
 uint8_t m = 24;
 
 void loop() {
-  char m_str[3];
-strcpy(m_str, u8x8_u8toa(m, 2));		/* convert m to a string with two digits */
+char m_str[3];
+
 u8g2.firstPage();
 do {
   u8g2_prepare();
-   //u8g2_box_title(0);
    draw_hexagon(70);
+
+    strcpy(m_str, u8x8_u8toa(encoderPos, 2));		/* convert m to a string with two digits */
+    u8g2.drawStr(10, 5, m_str);
+  if (oldEncPos != encoderPos) {
+    oldEncPos = encoderPos;
+  }
 } while ( u8g2.nextPage() );
 
 }
